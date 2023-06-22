adb90b1bb514dc4cc4c5763ca4973ade542efdaa
DipongkarRoy770 <dipongkarroy233@gmail.com>
2023-06-22 18:09:27 +0600

    nice

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -1,11 +1,16 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigation } from "react-router-dom";
 import Navbar from "../pages/Navbar";
 import Footer from "../pages/Footer";
 
 const Main = () => {
+  const navigation = useNavigation();
+
   return (
     <>
       <Navbar></Navbar>
+      <div>
+      {navigation.state === 'loading' ?<span className="bg-white loading loading-bars loading-lg"></span>:''}
+     </div>
       <Outlet></Outlet>
       <Footer></Footer>
     </>
